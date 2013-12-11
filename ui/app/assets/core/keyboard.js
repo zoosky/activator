032aa16c27bd6aa48a3ae739a8b404c227f21a52
Havoc Pennington <hp@pobox.com>
2013-12-10 20:08:52 -0500

    use keymage for 't' global binding
    - combine the two jquery bindings into one
    - change keyboard.js to be a keymage wrapper
    - factor out binding array convenience API
      from pluginapi to use globally also

--- ui/app/assets/core/keyboard.js
+++ ui/app/assets/core/keyboard.js
@@ -1,11 +1,32 @@
-define(function() {
-
-  $(window).on("keyup", function(e) {
-    switch (e.keyCode) {
-      case 84: // T
-        $.event.trigger("search.open");
-        break;
+// this file is a simple keymage wrapper to control our usage of it
+// and offer some convenience functionality.
+define([ 'webjars!keymage' ], function(key) {
+  return {
+    installBindingsInScope: function(scope, keybindings) {
+      $.each(keybindings, function(i, params) {
+        // we need to decide if there's a sub-scope in the parameter list,
+        // which would look like key('scope', 'ctrl-c', function(){})
+        var adjusted = null;
+        if (params.length > 2 && typeof(params[2]) == 'function') {
+          adjusted = params.slice(0);
+          adjusted[0] = scope + '.' + params[0];
+        } else {
+          adjusted = params.slice(0);
+          adjusted.unshift(scope);
+        }
+        console.log("in scope " + scope + " creating keybinding ", adjusted);
+        key.apply(null, adjusted);
+      });
+    },
+    pushScope: function(scope) {
+      console.log("pushing scope " + scope);
+      key.pushScope(scope);
+      console.log("key scope is now: " + key.getScope());
+    },
+    popScope: function(scope) {
+      console.log("popping scope " + scope);
+      key.popScope(scope);
+      console.log("key scope is now: " + key.getScope());
     }
-  });
-
+  };
 });
