cb4403837ed458191d68dc070bfd3b04afcd234e
Maxime DANTEC <hello@warry.fr>
2013-12-12 10:04:18 +0100

    Shortcut improvements

--- ui/app/assets/services/build.js
+++ ui/app/assets/services/build.js
@@ -2,10 +2,18 @@
  Copyright (C) 2013 Typesafe, Inc <http://typesafe.com>
  */
 define(['webjars!knockout','commons/settings'], function(ko,settings){
+
+  var startApp = ko.observable( settings.get("build.startApp", true) );
+  var rerunOnBuild = ko.observable( settings.get("build.rerunOnBuild", true) );
+  var runInConsole = ko.observable( settings.get("build.runInConsole", true) );
+  var consoleOn = ko.computed(function(){ return startApp() && runInConsole() });
+  var retestOnSuccessfulBuild = ko.observable( settings.get("build.retestOnSuccessfulBuild", true) );
+
   return {
-    startApp: ko.observable( settings.get("build.startApp", true) ),
-    rerunOnBuild: ko.observable( settings.get("build.rerunOnBuild", true) ),
-    runInConsole: ko.observable( settings.get("build.runInConsole", false) ),
-    retestOnSuccessfulBuild: ko.observable( settings.get("build.retestOnSuccessfulBuild", false) )
+    startApp: startApp,
+    rerunOnBuild: rerunOnBuild,
+    runInConsole: runInConsole,
+    consoleOn: consoleOn,
+    retestOnSuccessfulBuild: retestOnSuccessfulBuild
   }
 });
