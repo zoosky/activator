53e715d244f8745d347911160d5e313b0a140b3a
Havoc Pennington <hp@pobox.com>
2014-01-15 16:22:58 -0500

    Move most of run.js over to build.js

--- ui/app/assets/services/build.js
+++ ui/app/assets/services/build.js
@@ -233,12 +233,390 @@ define(['webjars!knockout', 'commons/settings', 'widgets/log/log', 'commons/util
     }
   });
 
+  var run = utils.Singleton({
+    init: function() {
+      var self = this;
+
+      this.activeTask = ko.observable(""); // empty string or taskId
+      this.haveActiveTask = ko.computed(function() {
+        return self.activeTask() != "";
+      }, this);
+      this.mainClasses = ko.observableArray();
+      // note: we generally want to use the explicitly-set defaultMainClass even if it
+      // isn't in the list of discovered mainClasses
+      this.defaultMainClass = ko.observable("");
+      this.currentMainClass = ko.observable("");
+      this.haveMainClass = ko.computed(function() {
+        // when we set mainClasses to empty list (as it is by default), knockout will set
+        // currentMainClass to 'undefined'
+        return typeof(self.currentMainClass()) == 'string' && self.currentMainClass() != "";
+      }, this);
+      this.haveActiveTask.subscribe(function(active) {
+        if (!active) events.send({ 'type' : 'RunStopped' });
+      });
+      this.restartPending = ko.observable(false);
+      this.reloadMainClassPending = ko.observable(true);
+      // last task ID we tried to stop
+      this.stoppingTaskId = '';
+
+      events.subscribe(function(event) {
+        return event.type == 'CompileSucceeded';
+      },
+      function(event) {
+        self.onCompileSucceeded(event);
+      });
+
+      this.playAppLink = ko.observable('');
+      this.playAppStarted = ko.computed(function() { return self.haveActiveTask() && self.playAppLink() != ''; }, this);
+      this.atmosLink = ko.observable('');
+
+      this.status = ko.observable('Application is stopped.');
+      this.outputLog = new logModule.Log();
+    },
+    loadMainClasses: function(success, failure) {
+      var self = this;
+
+      // the spaghetti here is getting really, really bad.
+      function taskCompleteShouldWeAbort() {
+        if (!self.haveActiveTask())
+          debug && console.log("BUG should not call this without an active task");
+        var weWereStopped = (self.activeTask() == self.stoppingTaskId);
+
+        // clear out our task always
+        self.activeTask('');
+
+        if (self.restartPending()) {
+          debug && console.log("Need to start over due to restart");
+          log.debug("Restarting...");
+          self.restartPending(false);
+          self.loadMainClasses(success, failure);
+          // true = abort abort
+          return true;
+        } else if (weWereStopped) {
+          debug && console.log("Stopped, restart not requested");
+          log.debug("Stopped");
+          // true = abort abort
+          return true;
+        } else {
+          // false = continue
+          return false;
+        }
+      }
+
+      debug && console.log("launching discoveredMainClasses task");
+      log.debug("launching discoveredMainClasses task");
+      var taskId = sbt.runTask({
+        task: 'discovered-main-classes',
+        onmessage: function(event) {
+          debug && console.log("event discovering main classes", event);
+          log.event(event);
+        },
+        success: function(data) {
+          debug && console.log("discovered main classes result", data);
+
+          if (taskCompleteShouldWeAbort())
+            return;
+
+          var names = [];
+          if (data.type == 'GenericResponse') {
+            names = data.params.names;
+            log.debug("Discovered main classes: " + names);
+          } else {
+            log.debug("No main classes discovered");
+          }
+          log.debug("Got auto-discovered main classes, looking for a default mainClass setting if any");
+          function noDefaultMainClassLogging(message) {
+            if (names.length > 0) {
+              log.debug("Didn't find a default mainClass setting, we'll just pick one of: " + names);
+            } else {
+              if (message)
+                log.error(message);
+              log.error("Didn't auto-discover a main class, and no mainClass was set");
+            }
+          }
+          debug && console.log("launching mainClass task");
+          log.debug("launching mainClass task");
+          var taskId = sbt.runTask({
+            task: 'main-class',
+            onmessage: function(event) {
+              debug && console.log("event getting default main class", event);
+              log.event(event);
+            },
+            success: function(data) {
+              debug && console.log("default main class result", data);
+
+              if (taskCompleteShouldWeAbort())
+                return;
+
+              var name = '';
+              // 'name' won't be in here if mainClass was unset
+              if (data.type == 'GenericResponse' && 'name' in data.params) {
+                name = data.params.name;
+                log.debug("Default main class is '" + name + "'");
+              } else {
+                // this isn't what really happens if it's not configured, I think
+                // sbt just tries to ask the user to pick, which fails, and we
+                // get the failure callback. But log just in case.
+                noDefaultMainClassLogging();
+              }
+              success({ name: name, names: names });
+            },
+            failure: function(status, message) {
+              // a common reason for fail is that sbt tried to ask /dev/null to
+              // pick a main class manually.
+              debug && console.log("getting default main class failed", message);
+
+              if (taskCompleteShouldWeAbort())
+                return;
+
+              noDefaultMainClassLogging();
+              // we don't treat this as failure, just as no default set
+              success({ name: '', names: names });
+            }
+          });
+          self.activeTask(taskId);
+        },
+        failure: function(status, message) {
+          debug && console.log("getting main classes failed", message);
+
+          if (taskCompleteShouldWeAbort())
+            return;
+
+          log.debug("Failed to discover main classes: " + message);
+          failure(status, message);
+        }
+      });
+      self.activeTask(taskId);
+    },
+    onCompileSucceeded: function(event) {
+      var self = this;
+
+      debug && console.log("Compile succeeded - marking need to reload main class info");
+      self.reloadMainClassPending(true);
+      if (settings.build.rerunOnBuild()) {
+        debug && console.log("Restarting due to completed compile");
+        self.doRestart();
+      } else {
+        debug && console.log("Run-on-compile not enabled, but we want to load main classes to fill in the option menu.");
+        self.doMainClassLoadThenMaybeRun(false /* shouldWeRun */);
+      }
+    },
+    beforeRun: function() {
+      var self = this;
+      if (self.reloadMainClassPending()) {
+        log.info("Loading main class information...");
+        self.status('Loading main class...');
+      } else {
+        self.status('Running...');
+        log.info("Running...");
+      }
+
+      self.restartPending(false);
+    },
+    doRunWithMainClassLoad: function() {
+      this.doMainClassLoadThenMaybeRun(true /* shouldWeRun */);
+    },
+    doMainClassLoadThenMaybeRun: function(shouldWeRun) {
+      var self = this;
+
+      // we clear logs here then ask doRunWithoutMainClassLoad not to.
+      log.clear();
+
+      self.beforeRun();
+
+      // whether we get main classes or not we'll try to
+      // run, but get the main classes first so we don't
+      // fail if there are multiple main classes.
+      function afterLoadMainClasses() {
+        self.reloadMainClassPending(false);
+
+        if (shouldWeRun) {
+          log.debug("Done loading main classes - now running the project");
+          self.doRunWithoutMainClassLoad(false /* clearLogs */);
+        }
+      }
+
+      // update our list of main classes
+      this.loadMainClasses(function(data) {
+        // SUCCESS
+        debug && console.log("GOT main class info ", data);
+
+        self.defaultMainClass(data.name);
+        debug && console.log("Set default main class to " + self.defaultMainClass());
+        // ensure the default configured class is in the menu
+        if (self.defaultMainClass() != '' && data.names.indexOf(self.defaultMainClass()) < 0)
+          data.names.push(self.defaultMainClass());
+
+        // when we set mainClasses, knockout will immediately also set currentMainClass to one of these
+        // due to the data binding on the option menu.
+        var actualCurrent = self.currentMainClass();
+        if (typeof(actualCurrent) == 'undefined')
+          actualCurrent = '';
+        var newCurrent = '';
+
+        debug && console.log("Current main class was: '" + actualCurrent + "'");
+        // so here's where knockout makes currentMainClass into something crazy
+        self.mainClasses(data.names);
+        debug && console.log("Set main class options to " + self.mainClasses());
+
+        // only force current selection to change if it's no longer
+        // discovered AND no longer explicitly configured in the build.
+        if (actualCurrent != '' && self.mainClasses().indexOf(actualCurrent) >= 0) {
+          newCurrent = actualCurrent;
+          debug && console.log("Keeping current main class since it still exists: '" + newCurrent + "'");
+        }
+
+        // if no existing setting, try to set it
+        if (newCurrent == '') {
+          if (self.defaultMainClass() != '') {
+            debug && console.log("Setting current main class to the default " + self.defaultMainClass());
+            newCurrent = self.defaultMainClass();
+          } else if (self.mainClasses().length > 0) {
+            debug && console.log("Setting current main class to the first in our list");
+            newCurrent = self.mainClasses()[0];
+          } else {
+            debug && console.log("We have nothing to set the current main class to");
+            newCurrent = '';
+          }
+        }
+
+        debug && console.log("Current main class is now: '" + newCurrent + "'");
+        self.currentMainClass(newCurrent);
+
+        afterLoadMainClasses();
+      },
+      function(status, message) {
+        // FAIL
+        debug && console.log("FAILED to set up main classes");
+        afterLoadMainClasses();
+      });
+    },
+    doAfterRun: function() {
+      var self = this;
+      self.activeTask("");
+      self.playAppLink("");
+      self.atmosLink("");
+      if (self.restartPending()) {
+        self.doRun();
+      }
+    },
+    doRunWithoutMainClassLoad: function(clearLogs) {
+      var self = this;
+
+      self.outputLog.clear();
+
+      if (clearLogs)
+        log.clear();
+
+      self.beforeRun();
+
+      // as a special-case, exclude Play's main class since running
+      // it "by hand" with run-main does not work. But people can
+      // still pick it in the dropdown to mean "play run"
+      var mainClassIsPlayServer =
+        self.currentMainClass() == 'play.core.server.NettyServer';
+      if (mainClassIsPlayServer)
+        log.debug("Using 'run' rather than 'run-main' for Play's server class")
+
+      var task = {};
+      if (self.haveMainClass() && !mainClassIsPlayServer) {
+        task.task = 'run-main';
+        task.params = { mainClass: self.currentMainClass() };
+      } else {
+        task.task = 'run';
+      }
+
+      if (settings.build.runInConsole()) {
+        task.task = 'atmos:' + task.task;
+      }
+
+      debug && console.log("launching " + task.task + " task");
+      var taskId = sbt.runTask({
+        task: task,
+        onmessage: function(event) {
+          if (event.type == 'LogEvent') {
+            var logType = event.entry.type;
+            if (logType == 'stdout' || logType == 'stderr') {
+              self.outputLog.event(event);
+            } else {
+              log.event(event);
+            }
+          } else if (event.type == 'Started') {
+            // our request went to a fresh sbt, and we witnessed its startup.
+            // we may not get this event if an sbt was recycled.
+            // we move "output" to "logs" because the output is probably
+            // just sbt startup messages that were not redirected.
+            log.moveFrom(self.outputLog);
+          } else if (event.id == 'playServerStarted') {
+            var port = event.params.port;
+            var url = 'http://localhost:' + port;
+            self.playAppLink(url);
+          } else if (event.id == 'atmosStarted') {
+            self.atmosLink(event.params.uri);
+            events.send({ 'type' : 'AtmosStarted' });
+          } else {
+            log.leftoverEvent(event);
+          }
+        },
+        success: function(data) {
+          debug && console.log("run result: ", data);
+          if (data.type == 'GenericResponse') {
+            log.info('Run complete.');
+            self.status('Run complete');
+          } else {
+            log.error('Unexpected reply: ' + JSON.stringify(data));
+          }
+          self.doAfterRun();
+        },
+        failure: function(status, message) {
+          debug && console.log("run failed: ", status, message)
+          self.status('Run failed');
+          log.error("Failed: " + status + ": " + message);
+          self.doAfterRun();
+        }
+      });
+      self.activeTask(taskId);
+    },
+    doRun: function() {
+      if (this.reloadMainClassPending())
+        this.doRunWithMainClassLoad();
+      else
+        this.doRunWithoutMainClassLoad(true /* clearLogs */);
+    },
+    doStop: function() {
+      var self = this;
+      if (self.haveActiveTask()) {
+        self.stoppingTaskId = self.activeTask();
+        sbt.killTask({
+          taskId: self.activeTask(),
+          success: function(data) {
+            debug && console.log("kill success: ", data);
+          },
+          failure: function(status, message) {
+            debug && console.log("kill failed: ", status, message)
+            self.status('Unable to stop');
+            log.error("HTTP request to kill task failed: " + message)
+          }
+        });
+      }
+    },
+    doRestart: function() {
+      if (this.haveActiveTask()) {
+        this.doStop();
+        this.restartPending(true);
+      } else {
+        this.doRun();
+      }
+    }
+  });
+
   var build = utils.Singleton({
     init: function() {
     },
     log: log,
     app: app,
-    compile: compile
+    compile: compile,
+    run: run
   });
 
   return build;
