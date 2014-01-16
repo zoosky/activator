a2d1707ee1cf3c5ab66cc108e3416ead79b053ea
James Ward <james@jamesward.org>
2014-01-16 14:51:32 -0700

    make template fetch timeout configurable and handle failure

--- launcher/src/main/scala/activator/ActivatorCli.scala
+++ launcher/src/main/scala/activator/ActivatorCli.scala
@@ -8,28 +8,56 @@ import activator.properties.ActivatorProperties.SCRIPT_NAME
 import activator.cache.Actions.cloneTemplate
 import java.io.File
 import sbt.complete.{ Parser, Parsers }
-import scala.concurrent.Await
+import scala.concurrent.{ TimeoutException, Await }
 import scala.concurrent.duration._
 import akka.actor.ActorSystem
-import activator.TemplatePopularityContest
+import scala.Some
+import com.typesafe.config.ConfigFactory
+import scala.collection.JavaConverters._
 
 object ActivatorCli {
   def apply(configuration: AppConfiguration): Int = try withContextClassloader {
     System.out.println()
     val name = getApplicationName()
-    val system = ActorSystem()
+
+    val akkaConfig = ConfigFactory.parseMap(Map("akka.log-dead-letters" -> "off", "akka.log-dead-letters-during-shutdown" -> "off").asJava)
+
+    val system = ActorSystem("default", akkaConfig)
     val projectDir = new File(name).getAbsoluteFile
     // Ok, now we load the template cache...
 
-    // TODO - Configurable durations in some config file somewhere.
-    val defaultDuration = Duration(6, SECONDS)
+    val defaultDuration: FiniteDuration = sys.props.get("activator.timeout").flatMap { timeoutString =>
+      val duration = Duration(timeoutString)
+      if (duration.isFinite()) {
+        Some(FiniteDuration(duration.length, duration.unit))
+      } else {
+        None
+      }
+    } getOrElse Duration(10, SECONDS)
+
     implicit val timeout = akka.util.Timeout(defaultDuration)
+
     // Create our default cache
     // TODO - move this into a common shared location between CLI and GUI.
     val cache = UICacheHelper.makeDefaultCache(system)
     // Get all possible names.
     // TODO - Drive this whole thing through futures, if we feel SAUCY enough, rather than waiting for results.
-    val metadata = Await.result(cache.metadata, defaultDuration)
+    System.out.println()
+    System.out.println("Fetching the latest list of templates...")
+    System.out.println()
+    val metadata = try {
+      Await.result(cache.metadata, defaultDuration)
+    } catch {
+      case e: TimeoutException =>
+        // fall back to just using whatever we have in the local cache
+        System.out.println()
+        System.out.println("Could not fetch the updated list of templates.  Using the local cache.")
+        System.out.println("Check your proxy settings or increase the timeout.  For more details see:\nhttp://typesafe.com/activator/docs")
+        System.out.println()
+
+        val localOnlyCache = UICacheHelper.makeLocalOnlyCache(ActorSystem("fallback", akkaConfig))
+        Await.result(localOnlyCache.metadata, defaultDuration)
+    }
     val templateNames = metadata.map(_.name).toSeq.distinct
     System.out.println()
     System.out.println(s"The new application will be created in ${projectDir.getAbsolutePath}")
@@ -104,8 +132,8 @@ object ActivatorCli {
       import Parsers._
       token(any.* map { _ mkString "" }, "<template name>").examples(possible.toSet, false)
     }
-
-    System.out.println("Enter a template name, or hit tab to see a list of possible templates")
+    System.out.println("Browse the list of templates: http://typesafe.com/activator/templates")
+    System.out.println("Enter a template name, or hit tab to see a list")
     readLine(templateNameParser) filterNot (_.isEmpty) getOrElse sys.error("No template name specified.")
   }
 
--- ui-common/src/main/scala/activator/UICacheHelper.scala
+++ ui-common/src/main/scala/activator/UICacheHelper.scala
@@ -33,6 +33,22 @@ object UICacheHelper {
       seedRepository = localSeed)
   }
 
+  def makeLocalOnlyCache(actorFactory: ActorRefFactory)(implicit timeout: akka.util.Timeout): TemplateCache = {
+    // TODO - Config or ActiavtorProperties?
+    val config = ConfigFactory.load()
+    val log = actorFactory match {
+      case system: ActorSystem => system.log
+      case context: ActorContext => context.system.log
+      case whatever => throw new RuntimeException(s"don't know how to get log from $whatever")
+    }
+    val localCache = new File(ActivatorProperties.ACTIVATOR_TEMPLATE_CACHE)
+    val localSeed = Option(ActivatorProperties.ACTIVATOR_TEMPLATE_LOCAL_REPO) map (new File(_)) filter (_.isDirectory)
+    DefaultTemplateCache(
+      actorFactory = actorFactory,
+      location = localCache,
+      seedRepository = localSeed)
+  }
+
   /** Grabs the additional script files we should clone with templates, if they are available in our environment. */
   def scriptFilesForCloning: Seq[(File, String)] = {
     def fileFor(loc: String, name: String): Option[(File, String)] = Option(loc) map (new File(_)) filter (_.exists) map (_ -> name)
