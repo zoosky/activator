b82173df53d387751285a218d6ded9e443c16322
Maxime DANTEC <hello@warry.fr>
2014-01-22 21:47:06 +0100

    Dynamic code browser almost done

--- ui/app/assets/plugins/code/code.js
+++ ui/app/assets/plugins/code/code.js
@@ -11,7 +11,9 @@ define([
   browser
 ){
 
-  var tree = fs.tree();
+  var tree;
+  window.tree = tree;
+  var reloadTree = ko.observable(0);
   var openedDocuments = ko.observableArray([]);
   var selectedDocument = ko.observable();
   var visible = ko.computed(function(){
@@ -20,11 +22,12 @@ define([
 
   var documentState = function(doc){
     var self = this;
-    this.title = doc.title;
-    this.location = doc.location;
+    this.name = doc.name;
+    this.location = doc.location.replace(serverAppModel.location, "");
     this.active = ko.observable(false);
     this.scroll = ko.observable(0);
     this.body = ko.observable("");
+    this.edited = ko.observable(false);
     this.working = ko.observable(false);
     this.undo = []; // Ace undo history
 
@@ -32,6 +35,7 @@ define([
     this.save = function(callback){
       self.working(true);
       fs.save(self.location, self.body(), function() {
+        self.edited(false);
         self.working(false);
       });
     }
@@ -41,6 +45,7 @@ define([
       self.working(true);
       fs.get(self.location, function(data) {
         self.working(false);
+        self.edited(false);
         self.body(data);
       });
     this.restore();
@@ -56,8 +61,96 @@ define([
     }
   }
 
+  var getStringAfterLastSlash = function(str) {
+    var splited = str.split("/");
+    return splited[splited.length-1];
+  }
+
+  // Recursively parse the tree
+  var findInTree = function(path, branch) {
+    // We still have one slice of the path to inspect
+    if (path.length > 0 && path[0] !== null) {
+      var current = path.shift(), next;
+      if (current === ""){
+        return findInTree(path, branch);
+      } else if (branch){
+        branch.children = branch.children || [];
+        for (var i in branch.children){
+          next = branch.children[i];
+          if (next.name == current ) {
+            return findInTree(path, next);
+          }
+        }
+        return false;
+      } else {
+        return false;
+      }
+
+    // Mothing more to inpect... let's return the current branch
+    } else {
+      return branch;
+    }
+  }
+
+  var loadUrl = function(url){ // url is an object from the router
+    // Load all directories and files in the path
+    var path = [""].concat(url.parameters); // [""] is project root
+    var toLoad = [];
+
+    // Is it already loaded in tree or not?
+    var p;
+    for (var i in path) {
+      p = url.parameters.slice(0,i);
+      if (!findInTree(p.slice(0), tree)){
+        // Order here is important, we need parents to be parsed first
+        toLoad.push( fs.browse( [serverAppModel.location].concat(p).join("/") ) );
+      }
+    }
+
+    console.log(toLoad)
+    // Starts all Ajax request at the same time
+    if(toLoad.length) $.when.apply(null, toLoad).then(loadInTree);
+  }
+
+  var loadInTree = function(a){
+    var paths;
+    if (toString.call(a) == "[object Array]"){
+      var paths = [].slice.call(arguments).map(function(it) { return it[0]; });
+    } else {
+      paths = [a];
+    }
+    // get parent in tree
+    for (var i in paths) {
+      relativePath = paths[i].location.replace(serverAppModel.location, "").split("/");
+
+      // Not root folder
+      if (relativePath.length && tree) {
+        var branch = findInTree(relativePath.slice(0), tree);
+        // Put the children loaded from Ajax in the object
+        branch.children = paths[i].children;
+        // Also open the directory by default
+        branch.opened = ko.observable(true);
+
+      // Root folder
+      } else {
+        tree = paths[i];
+        tree.opened = ko.observable(true);
+      }
+    }
+    reloadTree(reloadTree()+1);
+  }
+
+  var toggleDir = function(dir) {
+      console.log(dir)
+    if (dir.children && dir.opened) {
+      dir.opened(!dir.opened);
+    } else if(dir) {
+      console.log({parameters: dir.location.replace(serverAppModel.location, "").split("/") })
+      loadUrl({parameters: dir.location.replace(serverAppModel.location, "").split("/") });
+    }
+  }
+
   var openFile = function(newdoc) {
-    console.log(newdoc)
     var foundDocIndex, doc;
     // Is it loaded already?
     for (var index in openedDocuments()){
@@ -77,6 +170,7 @@ define([
   var closeFile = function(newdoc, e) {
     e.preventDefault();
     e.stopPropagation();
+    if (newdoc.edited() && !confirm("Do you really want to close without saving?")) return false;
     var foundDocIndex, doc;
     for (var index in openedDocuments()){
       doc = openedDocuments()[index];
@@ -117,8 +211,12 @@ define([
   }
 
   var CodeState = {
-    tree: tree,
+    tree: ko.computed(function() {
+      reloadTree();
+      return tree;
+    }),
     openedDocuments: openedDocuments,
+    toggleDir: toggleDir,
     openFile: openFile,
     closeFile: closeFile,
     selectedDocument: selectedDocument,
@@ -139,12 +237,17 @@ define([
       var all = [['code/', "Code"]];
       breadcrumb(all.concat([["code/"+url.parameters.join("/"),url.parameters.join("/")]]));
       if (url.parameters[0]){
+        // Load all directories and files in the path
+        loadUrl(url);
+
         openFile({
-          title: url.parameters[url.parameters.length-1],
+          name: url.parameters[url.parameters.length-1],
           location: "/"+url.parameters.join("/")
         });
       } else if (!selectedDocument()){
         breadcrumb(all);
+        loadUrl(url);
+        // $.when(fs.browse(serverAppModel.location)).then(loadInTree);
         window.location.hash = "code/";
       }
     }
