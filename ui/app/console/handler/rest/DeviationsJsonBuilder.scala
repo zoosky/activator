9b89f0d627bb3d8f6029d85e2df27310b5657dd8
he <henrik.engstrom@gmail.com>
2014-02-18 14:40:37 -0500

    Deviations works

--- ui/app/console/handler/rest/DeviationsJsonBuilder.scala
+++ ui/app/console/handler/rest/DeviationsJsonBuilder.scala
@@ -5,12 +5,13 @@ package console.handler.rest
 
 import akka.actor.ActorRef
 import console.ClientController.Update
+import play.api.libs.json.{ JsValue, JsArray, Json, JsObject }
 
 class DeviationsJsonBuilder extends JsonBuilderActor {
   import DeviationsJsonBuilder._
 
   def receive = {
-    case r: DeviationsResult => //r.receiver ! Update(null)
+    case r: DeviationsResult => r.receiver ! Update(createJson(r.result))
   }
 }
 
@@ -18,4 +19,45 @@ object DeviationsJsonBuilder {
   import activator.analytics.data._
 
   case class DeviationsResult(receiver: ActorRef, result: Either[Seq[ErrorStats], Seq[ActorStats]])
+
+  def createJson(result: Either[Seq[ErrorStats], Seq[ActorStats]]): JsObject =
+    Json.obj(
+      "type" -> "deviations",
+      "data" ->
+        Json.obj(
+          "deviations" -> handleTypes(result)))
+
+  private def handleTypes(result: Either[Seq[ErrorStats], Seq[ActorStats]]): JsValue = result match {
+    case Left(x) => generateErrorStats(x)
+    case Right(x) => generateActorStats(x)
+  }
+
+  private def generateErrorStats(stats: Seq[ErrorStats]): JsValue =
+    Json.toJson(stats.map(i =>
+      Json.obj(
+        "errorCount" -> i.metrics.counts.errors,
+        "errors" -> extract(i.metrics.deviations.errors),
+        "warningCount" -> i.metrics.counts.warnings,
+        "warnings" -> extract(i.metrics.deviations.warnings),
+        "deadletterCount" -> i.metrics.counts.deadLetters,
+        "deadletters" -> extract(i.metrics.deviations.deadLetters),
+        "unhandledMessageCount" -> i.metrics.counts.unhandledMessages,
+        "unhandledMessages" -> extract(i.metrics.deviations.unhandledMessages),
+        "deadlockCount" -> i.metrics.counts.deadlocks,
+        "deadlocks" -> extract(i.metrics.deviations.deadlockedThreads))))
+
+  private def extract(details: List[DeviationDetail]): List[JsObject] = {
+    details.map(d =>
+      Json.obj(
+        "event" -> d.eventId.toString,
+        "trace" -> d.traceId.toString,
+        "message" -> d.message,
+        "timestamp" -> d.timestamp))
+  }
+
+  private def generateActorStats(stats: Seq[ActorStats]): JsValue = {
+    Json.toJson(stats.map(i =>
+      Json.obj(
+        "node" -> "")))
+  }
 }
