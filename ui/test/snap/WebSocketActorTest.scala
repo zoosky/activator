7cd10e32e2645faf687294e77d46d40b9238f46d
Jim Powers <jim@corruptmemory.com>
2014-03-19 13:30:07 -0400

    Added failure case to make sure ;-)

--- ui/test/snap/WebSocketActorTest.scala
+++ ui/test/snap/WebSocketActorTest.scala
@@ -12,5 +12,10 @@ class WebSocketActorTest {
     val message1Json = Json.toJson(message1)
     val message1Return = message1Json.as[InspectRequest]
     assertTrue("InspectRequest message makes JSON round trip", message1 == message1Return)
+
+    val message2 = InspectRequest(Json.obj("foo" -> "bar"))
+    val message2Json = Json.toJson(message2).asInstanceOf[JsObject] ++ Json.obj("request" -> "NotInspectRequest")
+    val message2Return = Json.fromJson[InspectRequest](message2Json)
+    assertTrue("Not parse a request for InspectRequest if not tagged appropriately", message2Return.isInstanceOf[JsError])
   }
 }
